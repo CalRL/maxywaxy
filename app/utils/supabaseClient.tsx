59e5fffedf95cebe46ba61438e579380f0a0f8d4
Cal <callumburnsoregan@gmail.com>
2024-10-06 15:58:43 +0100

    fix env

--- app/utils/supabaseClient.tsx
+++ app/utils/supabaseClient.tsx
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 dotenv.config();
 
-const supabaseURL: string = process.env.SUPABASE_URL!;
-const supabaseKey: string = process.env.SUPABASE_API_KEY!;
+const supabaseURL: string = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_API_KEY!;
 
 export const supabase = createClient(supabaseURL, supabaseKey);
